c46a764034efcbddea8abebed5bdbfd0cd614e4b
jackdevelop@sina.com <jackdevelop@sina.com>
2014-04-03 10:21:44 +0800

    test

--- client/src/run.bat
+++ client/src/run.bat
@@ -1,4 +1,4 @@
 @echo off
 SET SCRIPTS_DIR=%~dp0
 
-START /B %SCRIPTS_DIR%\..\player\bin\win32\quick-x-player.exe -workdir %SCRIPTS_DIR%\ -file %SCRIPTS_DIR%\scripts\main.lua -size 960x640
+START /B %SCRIPTS_DIR%\proj.win32\Debug\butterfly-player.exe -workdir %SCRIPTS_DIR%\ -file %SCRIPTS_DIR%\scripts\main.lua -size 960x640
